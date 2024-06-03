3bdfc98eaf5935b24b7bc0b6d10e2904182b9fa5
Redact <andreiblue2016@gmail.com>
2024-06-03 19:20:43 +0800

    Set up server.js and add all routers

---
+++ Server.js
@@ -0,0 +1,42 @@
+const createError = require('http-errors');
+const path = require("path");
+const express = require("express");
+
+//Connecting to MongoDB.
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+//mongoose.connect() //Connect.
+
+const programsRouter = require("./routes/programs");
+const beneficiaryRouter = require("./routes/beneficiaries");
+const benefactorRouter = require("./routes/programs");
+const inventoryRouter = require("./routes/programs");
+
+const app = new express();
+app.use(express.json());
+app.use(express.urlencoded( {extended: true})); 
+app.use(express.static(__dirname + '/public'));
+
+//Setting up routers.
+app.use("/programs", programsRouter);
+app.use("/beneficiaries", beneficiaryRouter);
+app.use("/benefactors", benefactorRouter);
+app.use("/inventory", inventoryRouter);
+
+const hbs = require("hbs");
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+hbs.registerHelper('toString', function(objectId) {
+    return objectId.toString();
+});
+
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+const server = app.listen(3000, function() {
+    console.log("Running at Node 3000");
+});
+
+module.exports = app;
