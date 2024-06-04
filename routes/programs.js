9276876b6b07951d5c4a2969c8be38bc975e5387
AndreiPalonpon <andrei_palonpon@dlsu.edu.ph>
2024-06-04 11:51:00 +0800

    Add all required requests for each of the routers

--- routes/programs.js
+++ routes/programs.js
@@ -1,9 +1,44 @@
 const express = require("express");
 const router = express.Router();    
 
-//GET 
-router.get('/', function (req, res, next) {
+//GET request for creating program.
+router.get('/create', asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Program create GET");
+}));
 
-});
+//POST request for creating program.
+router.post('/create', asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Program create POST");
+}));
+
+//GET request for editing program.
+router.get('/:id/edit', asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Program edit GET");
+}));
+
+//POST request for editing program.
+router.post('/:id/edit', asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Program edit POST");
+}));
+
+//GET request for deleting program. 
+router.get('/:id/delete', asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Program delete GET");
+}));
+
+//POST request for deleting program. 
+router.post('/:id/delete', asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Program delete POST");
+}));
+
+//GET request to list all programs.
+router.get('/', asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Program list");
+}));
+
+//GET request for one program.
+router.get('/:id', asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Program detail");
+}));
 
 module.exports = router;
