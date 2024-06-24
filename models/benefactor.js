3b367aec0cdfd77719ab9cd0e2fe2216e200e9d1
NeoMonserrat <antonio_monserrat@dlsu.edu.ph>
2024-06-24 09:04:27 +0800

    constructed benefactor schema

--- models/benefactor.js
+++ models/benefactor.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BenefactorScheme = new Schema({
-    
+    benefactor_name: String,
+    benefactor_type: {
+        type: String,
+        required: true,
+        enum: ["", "", "", "",],
+        default: "Benefactor"
+    },
+    benefactor_contribution: String,
+    programs_helped: String,
 });
 
 module.exports = mongoose.model("Benefactor", BenefactorScheme, "benefactors");
