62cbda872ecdd61d7983a6d93138121c8eeee661
NeoMonserrat <antonio_monserrat@dlsu.edu.ph>
2024-06-28 20:37:44 +0800

    switched required to true

--- models/reworkedModels/device.js
+++ models/reworkedModels/device.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const DeviceSchema = new Schema({
     name: { type: String, required: false },
     description: { type: String, required: false },
-    date_received: { type: Date, required: false }, //switch to true
-    sponsor: { type: String, required: false },
+    date_received: { type: Date, required: true }, 
+    sponsor: { type: String, required: true },
     
     /*sponsor: {
         type: Schema.Types.ObjectId, 
