7fe0d1896d958c39827b2e50f22ab71f8611195b
NeoMonserrat <antonio_monserrat@dlsu.edu.ph>
2024-06-28 22:48:24 +0800

    Fixed comment

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-//GET login page.
+//GET request about page
 router.get("/", function (req, res) {
     res.sendFile(path.resolve('./views/about.html'));
 });
