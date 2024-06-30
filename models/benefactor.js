06bb259b87a052f1124fff74ad7729c0e87aa96b
AndreiPalonpon <andrei_palonpon@dlsu.edu.ph>
2024-06-30 08:44:29 +0800

    Rename sponsor model to benefactor

---
+++ models/benefactor.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BenefactorSchema = new Schema({
+    name: { type: String, required: true, },
+    type: {
+        type: String,
+        required: true,
+        enum: ["", "", "", "",],
+    },
+});
+
+module.exports = mongoose.model("Benefactor", BenefactorSchema, "benefactors");
