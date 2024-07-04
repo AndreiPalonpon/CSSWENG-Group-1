f5c5bcc2aad878234f38b6afaa90cfe521545e14
angieolonan03 <giastkidding@gmail.com>
2024-07-04 21:13:29 +0800

    Updated routes/people.js and views/people-list.hbs: added barangay codes, modified people creation and listing routes, and updated people list template

--- routes/people.js
+++ routes/people.js
@@ -5,16 +5,39 @@ const Person = require("../models/person");
 const Program = require("../models/program"); //?
 
 
-const router = express.Router();    
+const router = express.Router();
+
+const barangayCodes = {
+    "001": "Almanza Uno",
+    "002": "Daniel Fajardo",
+    "003": "Elias Aldana",
+    "004": "Ilaya",
+    "005": "Manuyo Uno",
+    "006": "Pamplona Uno",
+    "007": "Pulanglupa Uno",
+    "008": "Talon Uno",
+    "009": "Zapote",
+    "010": "Almanza Dos",
+    "011": "BF International/CAA",
+    "012": "Manuyo Dos",
+    "013": "Pamplona Dos",
+    "014": "Pamplona Tres",
+    "015": "Pilar",
+    "016": "Pulanglupa Dos",
+    "017": "Talon Dos",
+    "018": "Talon Tres",
+    "019": "Talos Kuartro",
+    "020": "Talon Singko"
+};
 
 // GET home page.
 router.get('/', function(req, res, next) {
-    res.render('people-list', { title: 'People List' });
+    res.render('people-list', { title: 'People List', barangayCodes });
 });
 
 
 // GET request for fetching people list under a specific program
-router.get('/:id', asyncHandler(async (req, res, next) => {
+router.get('/:id', asyncHandler(async(req, res, next) => {
     try {
         // Fetch the program details by ID
         const program = await Program.findById(req.params.id);
@@ -29,7 +52,7 @@ router.get('/:id', asyncHandler(async (req, res, next) => {
         console.log('Program details:', program);
 
         // Render the people list template with the fetched data
-        res.render('people-list', { title: 'People List', program });
+        res.render('people-list', { title: 'People List', program, barangayCodes });
     } catch (err) {
         // Handle any errors that occur during fetching
         console.error('Error fetching program details:', err);
@@ -39,44 +62,39 @@ router.get('/:id', asyncHandler(async (req, res, next) => {
 
 
 //GET request for creating people.
-router.get('/create', asyncHandler(async (req, res, next) => {
+router.get('/create', asyncHandler(async(req, res, next) => {
     res.send("NOT IMPLEMENTED: People create GET");
 }));
 
 
 // POST request for creating people.
-router.post('/create', asyncHandler(async (req, res, next) => {
-    const name =  req.body.name;
-    const dateJoined = req.body.dateJoined; 
-    const beneficiaryStatus = req.body.beneficiaryStatus;
-    const benefitRequested = req.body.benefitRequested;
-    const benefitReceived = req.body.benefitReceived;
-    const dateReceived = req.body.dateReceived;
-
+router.post('/create', asyncHandler(async(req, res, next) => {
     const newPerson = new Person({
-        name: name,
-        dateJoined: dateJoined,
-        beneficiaryStatus: beneficiaryStatus,
-        benefitRequested: benefitRequested,
-        benefitReceived: benefitReceived,
-        dateReceived: dateReceived
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        gender: req.body.gender,
+        birthdate: req.body.birthdate,
+        address: req.body.address,
+        barangay: req.body.barangay,
+        contact_number: req.body.contact_number,
+        disability_type: req.body.disability_type,
+        disability: req.body.disability,
+        pwd_card_id_no: req.body.pwd_card_id_no,
+        recent_pwd_id_update_date: req.body.recent_pwd_id_update_date
     });
 
     await newPerson.save();
     res.redirect('/'); // Redirect to home page or any other appropriate page after saving.
 
-    console.log("New people instance saved.");
+    console.log("New person instance saved.");
 }));
 
 //GET request to list all people.
-router.get('/', asyncHandler(async (req, res, next) => {
-    const people = await Person.find()
-                                .sort({ first_name: 1, last_name: 1 })
-                                .exec();
-
+router.get('/', asyncHandler(async(req, res, next) => {
+    const people = await Person.find().sort({ first_name: 1, last_name: 1 }).exec();
     //res.send(allPrograms);
     //console.log(programs);
-    res.render("people-list", { people: people });
+    res.render("people-list", { people: people, barangayCodes });
 }));
 
 module.exports = router;
