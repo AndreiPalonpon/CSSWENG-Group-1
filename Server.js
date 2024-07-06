4c6574e905db5641244e22e37d4d284838eaeb24
NeoMonserrat <antonio_monserrat@dlsu.edu.ph>
2024-07-07 01:10:54 +0800

    Fixed comments and organization in server.js

--- Server.js
+++ Server.js
@@ -1,34 +1,18 @@
-const createError = require('http-errors');
 const express = require("express");
 const session = require('express-session');
-
-//Connecting to MongoDB.
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://andreipalonpon:Ftlpn7UeDQfci6uG@appcluster.00tewof.mongodb.net/main?retryWrites=true&w=majority&appName=AppCluster";
-
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-}
-
+const createError = require('http-errors');
 const path = require("path");
 const cookieParser = require('cookie-parser');
-const logger = require('morgan')
+const logger = require('morgan');
 const moment = require('moment');
-
-const Program = require("./models/program");
-const Person = require("./models/person");
-const Beneficiary = require("./models/beneficiary");
-const Benefactor = require("./models/benefactor");
-const Benefit = require("./models/benefit");
-const Documentation = require("./models/documentation");
+const hbs = require("hbs");
 
 const indexRouter = require("./routes/index");
-const loginRouter = require("./routes/login")
-const logoutRouter = require("./routes/logout")
-const forgotPasswordRouter = require("./routes/forgot-password")
-const resetPasswordRouter = require("./routes/reset-password")
+const loginRouter = require("./routes/login");
+const logoutRouter = require("./routes/logout");
+const forgotPasswordRouter = require("./routes/forgot-password");
+const resetPasswordRouter = require("./routes/reset-password");
 const programsRouter = require("./routes/programs");
 const beneficiaryRouter = require("./routes/beneficiaries");
 const benefactorRouter = require("./routes/benefactors");
@@ -39,21 +23,54 @@ const settingsRouter = require("./routes/settings");
 const aboutRouter = require("./routes/about");
 const contactRouter = require("./routes/contact");
 
-const app = new express();
+const Program = require("./models/program");
+const Person = require("./models/person");
+const Beneficiary = require("./models/beneficiary");
+const Benefactor = require("./models/benefactor");
+const Benefit = require("./models/benefit");
+const Documentation = require("./models/documentation");
+
+const app = express();
+const mongoDB = "mongodb+srv://andreipalonpon:Ftlpn7UeDQfci6uG@appcluster.00tewof.mongodb.net/main?retryWrites=true&w=majority&appName=AppCluster";
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+main().catch((err) => console.log(err));
+
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded( {extended: true}));
-app.use(cookieParser()); 
-app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-    secret: 'your-secret-key', // Change this to a secure key
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false } // Set secure to true if using HTTPS
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
 }));
 
-//Setting up routers.
+// View engine setup
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Register handlebars helpers
+hbs.registerHelper('toString', function (objectId) {
+  return objectId.toString();
+});
+
+hbs.registerHelper('incremented', function (index) {
+  index++;
+  return index;
+});
+
+hbs.registerHelper('formatDate', function (date) {
+  return moment(date).format('MM-DD-YYYY');
+});
+
+// Routers setup
 app.use("/", indexRouter);
 app.use("/login", loginRouter);
 app.use("/logout", logoutRouter);
@@ -69,29 +86,14 @@ app.use("/settings", settingsRouter);
 app.use("/about", aboutRouter);
 app.use("/contact", contactRouter);
 
-const hbs = require("hbs");
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
-hbs.registerHelper('toString', function(objectId) {
-    return objectId.toString();
+// 404 error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-hbs.registerHelper('incremented', function (index) {
-    index++;
-    return index;
-});
-
-hbs.registerHelper('formatDate', function (date) {
-    return moment(date).format('MM-DD-YYYY');
-});
-
-app.use(function(req, res, next) {
-    next(createError(404));
-});
-
-const server = app.listen(3000, function() {
-    console.log("Running at Node 3000");
+// Start the server
+const server = app.listen(3000, function () {
+  console.log("Running at Node 3000");
 });
 
 module.exports = app;
