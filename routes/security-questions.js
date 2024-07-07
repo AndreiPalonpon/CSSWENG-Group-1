bb28d3f423c51b00800dcabcb39c3ab6e47305e4
NeoMonserrat <antonio_monserrat@dlsu.edu.ph>
2024-07-07 13:34:38 +0800

    Added edit security questions page + routes

---
+++ routes/security-questions.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const Question = require("../models/question");
+
+// Session Authenticator
+function requireAuth(req, res, next) {
+    console.log("Checking authentication...");
+    if (req.session.user && req.session.user.authenticated) {
+        console.log("User is authenticated. Proceeding to security questions page...");
+        next(); 
+    } else {
+        console.log("User is not authenticated. Redirecting to login page...");
+        res.redirect('/login');
+    }
+}
+
+// GET request to display dashboard page
+router.get("/", requireAuth, function (req, res) {
+    res.sendFile(path.resolve('./views/security-questions.html'));
+});
+
+module.exports = router;
