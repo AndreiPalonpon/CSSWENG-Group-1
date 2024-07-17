c459964284116ce2afdabe5ae1e4f48cde871fae
NeoMonserrat <antonio_monserrat@dlsu.edu.ph>
2024-07-17 14:40:31 +0800

    Fixed create and delete function for people

--- routes/people.js
+++ routes/people.js
@@ -20,37 +20,54 @@ function requireAuth(req, res, next) {
 router.use(requireAuth);
 
 //GET request to list all people
-router.get('/', asyncHandler(async(req, res, next) => {
+router.get('/', asyncHandler(async(req, res) => {
     const people = await Person.find()
                                .sort({ first_name: 1, last_name: 1 })
                                .exec();
     
+
     console.log(people[0].age);
+    console.log(people);
     res.render("people-list", { people: people });
 }));
 
-const barangayCodes = {
-    "001": "Almanza Uno",
-    "002": "Daniel Fajardo",
-    "003": "Elias Aldana",
-    "004": "Ilaya",
-    "005": "Manuyo Uno",
-    "006": "Pamplona Uno",
-    "007": "Pulanglupa Uno",
-    "008": "Talon Uno",
-    "009": "Zapote",
-    "010": "Almanza Dos",
-    "011": "BF International/CAA",
-    "012": "Manuyo Dos",
-    "013": "Pamplona Dos",
-    "014": "Pamplona Tres",
-    "015": "Pilar",
-    "016": "Pulanglupa Dos",
-    "017": "Talon Dos",
-    "018": "Talon Tres",
-    "019": "Talos Kuartro",
-    "020": "Talon Singko"
-};
+// POST request for creating people.
+router.post('/create', asyncHandler(async (req, res) => {
+    const { 
+        firstName, 
+        lastName, 
+        gender, 
+        birthdate,
+        address,
+        barangay,
+        contactNumber,
+        disability,
+        disabilityType,
+        pwd_card_id_no,
+        recent_pwd_id_update_date
+     } = req.body;
+    
+    const newPerson = new Person({
+        first_name: firstName,
+        last_name: lastName,
+        gender,
+        birthdate,
+        address,
+        barangay,
+        contact_number: contactNumber,
+        disability_type: disabilityType,
+        disability,
+        pwd_card_id_no,
+        recent_pwd_id_update_date,
+    });
+
+    await newPerson.save();
+    
+    await Person.create(newPerson);
+    console.log("New person instance saved.");
+    res.sendStatus(201); 
+}));
+
 
 // POST request for updating person
 router.get('/edit', asyncHandler(async (req, res, next) => { //Change to post. POST will be used.
@@ -107,32 +124,11 @@ router.get('/:id', asyncHandler(async(req, res, next) => {
     }
 }));
 
-// POST request for creating people.
-router.post('/create', asyncHandler(async(req, res, next) => {
-    const newPerson = new Person({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        gender: req.body.gender,
-        birthdate: req.body.birthdate,
-        address: req.body.address,
-        barangay: req.body.barangay,
-        contact_number: req.body.contact_number,
-        disability_type: req.body.disability_type,
-        disability: req.body.disability,
-        pwd_card_id_no: req.body.pwd_card_id_no,
-        recent_pwd_id_update_date: req.body.recent_pwd_id_update_date
-    });
-
-    await newPerson.save();
-    
-
-    console.log("New person instance saved.");
-}));
 
 //POST request for deleting person
-router.post('/delete', asyncHandler(async (req, res, next) => {
-    await Program.deleteOne({_id: req.body.person_id});
-    console.log("Person ID " + req.body.person_id + " has been deleted.");
+router.post('/delete', asyncHandler(async (req, res) => {
+    await Person.deleteOne({ _id: req.body.person_id });
+    console.log(`Person ID ${req.body.person_id} has been deleted.`);
     res.sendStatus(200);
 }));
 
