7fc7bb875e0f137d589d8ad8a3d76be40db89316
angieolonan <144090845+angieolonan@users.noreply.github.com>
2024-07-19 15:52:17 +0800

    Update beneficiaries.js sort/filter get request

--- routes/beneficiaries.js
+++ routes/beneficiaries.js
@@ -21,28 +21,6 @@ function requireAuth(req, res, next) {
 
 router.use(requireAuth);
 
-// GET request to list all beneficiaries
-/*
-router.get('/', asyncHandler(async(req, res) => {
-    const people = await Person.find().sort({ first_name: 1, last_name: 1 }).exec();
-    const programs = await Program.find().sort({ name: 1 }).exec();
-    const benefits = await Benefit.find().sort({ name: 1 }).exec();
-    const beneficiaries = await Beneficiary.find()
-        .populate("person_registered")
-        .populate("program_enrolled")
-        .populate("benefit_delivered")
-        .exec();
-
-    console.log(beneficiaries);
-    res.render("beneficiary-list", {
-        people,
-        programs,
-        benefits,
-        beneficiaries
-    });
-}));
-*/
-
 // GET request to list all beneficiaries with sorting and filtering
 router.get('/', asyncHandler(async(req, res) => {
     const { recipientSort, statusFilter, benefitSort, dateSort, programId } = req.query;
@@ -51,15 +29,11 @@ router.get('/', asyncHandler(async(req, res) => {
     let filterOptions = { program_enrolled: programId };
 
     if (recipientSort) {
-        sortOptions['person_registered.name'] = recipientSort === 'az' ? 1 : -1;
+        sortOptions['person_registered.last_name'] = recipientSort === 'az' ? 1 : -1;
     }
 
     if (statusFilter) {
         filterOptions.status = { $in: statusFilter.split(',') };
-    }
-
-    if (benefitSort) {
-        sortOptions['benefit_delivered.name'] = benefitSort === 'az' ? 1 : -1;
     }
 
     if (dateSort) {
